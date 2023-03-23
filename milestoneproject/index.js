6ef6507b7f664e043fb7b1e6340614cfc33cb464
abhinavbansal30 <abhinav.bansal@calfus.com>
2023-03-23 13:08:37 +0530

    Changes in index final

--- milestoneproject/index.js
+++ milestoneproject/index.js
@@ -2,9 +2,7 @@ const express = require("express");
 const csv = require("csv-parser");
 const bodyParser = require("body-parser");
 const fs = require("fs");
-const cors = require('cors');
-
-
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
@@ -99,67 +97,69 @@ app.post("/upload-csv-file", (req, res) => {
   const year = req.query.year;
   let ans = [];
   {
-    lines.map(t=> {
-      if(t!=='') {
-      let e = t.split(",");
-      let flag = false;
-      const regex = /(?:[^,]*,){3}((?:[^,]*,)*[^,]*)/;
-          let milestone = ''
-          if(e.length > 5){
-            milestone = t.split(regex);
-            e[4] = e[e.length-1];
-            e[3] = milestone[1].match(/(?:"[^"]*"|^[^"]*$)/)[0].replace(/"/g, "");
-            e = e.slice(0,5)
-            // console.log(e);
-            flag = true;
-          }
-      console.log(e);
-      let dates1 = flag ? e[4] : e[4].toString();
+    lines.map(t => {
+      if (t !== "" && t.includes(",,")) {
+        let e = t.split(",");
+        let flag = false;
+        const regex = /(?:[^,]*,){3}((?:[^,]*,)*[^,]*)/;
+        let milestone = "";
+        if (e.length > 5) {
+          milestone = t.split(regex);
+          e[4] = e[e.length - 1];
+          e[3] = milestone[1].match(/(?:"[^"]*"|^[^"]*$)/)[0].replace(/"/g, "");
+          e = e.slice(0, 5);
+          // console.log(e);
+          flag = true;
+        }
+        console.log(e);
+        let dates1 = flag ? e[4] : e[4].toString();
 
-      if (dates1.includes("-")) {
-        const dates = dates1.split("-");
-        let c = dates[2].substring(0,dates[2].length-1)
-        var month1;
-        var year1;
-        if (dates[1] && isNaN(dates[1])) {
-          const sd = dates[1];
-          month1 = months[sd];
-          year1 = c?.length > 2 ? c : !c ? " " : c;
+        if (dates1.includes("-")) {
+          const dates = dates1.split("-");
+          let c = dates[2].substring(0, dates[2].length - 1);
+          var month1;
+          var year1;
+          if (dates[1] && isNaN(dates[1])) {
+            const sd = dates[1];
+            month1 = months[sd];
+            year1 = c?.length > 2 ? c : !c ? " " : c;
+          } else {
+            month1 =
+              dates[0]?.length === 1
+                ? `0${dates[0]}`
+                : !dates[0]
+                ? " "
+                : dates[0];
+            year1 = c?.length > 2 ? c : !c ? " " : c;
+          }
+          if (month === month1 && year === year1) {
+            ans.push(e);
+          }
         } else {
-          month1 =
-            dates[0]?.length === 1
-              ? `0${dates[0]}`
-              : !dates[0]
-              ? " "
-              : dates[0];
-          year1 = c?.length > 2 ? c : !c ? " " : c;;
-        }
-        if (month === month1 && year === year1) {
-          ans.push(e);
-        }
-      } else {
-        const dates = dates1.toString().split("/");
-        var month1;
-        var year1;
-        if (dates[1] && isNaN(dates[1])) {
-          const sd = dates[1];
-          month1 = months[sd];
-          year1 = dates[2]?.length > 2 ? dates[2] : !dates[2] ? " " : dates[2];
-        } else {
-          month1 =
-            dates[0]?.length === 1
-              ? `0${dates[0]}`
-              : !dates[0]
-              ? " "
-              : dates[0];
-          year1 = dates[2]?.length > 2 ? dates[2] : !dates[2] ? " " : dates[2];
-        }
-        if (month === month1 && year === year1) {
-          ans.push(e);
+          const dates = dates1.toString().split("/");
+          var month1;
+          var year1;
+          if (dates[1] && isNaN(dates[1])) {
+            const sd = dates[1];
+            month1 = months[sd];
+            year1 =
+              dates[2]?.length > 2 ? dates[2] : !dates[2] ? " " : dates[2];
+          } else {
+            month1 =
+              dates[0]?.length === 1
+                ? `0${dates[0]}`
+                : !dates[0]
+                ? " "
+                : dates[0];
+            year1 =
+              dates[2]?.length > 2 ? dates[2] : !dates[2] ? " " : dates[2];
+          }
+          if (month === month1 && year === year1) {
+            ans.push(e);
+          }
         }
       }
-    }
-  });
+    });
   }
 
   res.send(ans);
