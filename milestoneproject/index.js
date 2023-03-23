215b1c1f9ae8feb2d458c179b005b5fec0b7df7c
abhinavbansal30 <abhinav.bansal@calfus.com>
2023-03-23 12:59:57 +0530

    Changes in index.js

--- milestoneproject/index.js
+++ milestoneproject/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const csv = require("csv-parser");
-var cors = require("cors");
 const bodyParser = require("body-parser");
 const fs = require("fs");
+const cors = require('cors');
+
+
 
 const app = express();
 app.use(bodyParser.json());
@@ -97,17 +99,32 @@ app.post("/upload-csv-file", (req, res) => {
   const year = req.query.year;
   let ans = [];
   {
-    lines.data.map(e => {
-      var dates1 = Object.values(e)[4];
+    lines.map(t=> {
+      if(t!=='') {
+      let e = t.split(",");
+      let flag = false;
+      const regex = /(?:[^,]*,){3}((?:[^,]*,)*[^,]*)/;
+          let milestone = ''
+          if(e.length > 5){
+            milestone = t.split(regex);
+            e[4] = e[e.length-1];
+            e[3] = milestone[1].match(/(?:"[^"]*"|^[^"]*$)/)[0].replace(/"/g, "");
+            e = e.slice(0,5)
+            // console.log(e);
+            flag = true;
+          }
+      console.log(e);
+      let dates1 = flag ? e[4] : e[4].toString();
+
       if (dates1.includes("-")) {
-        const dates = dates1.toString().split("-");
-        //console.log(dates);
+        const dates = dates1.split("-");
+        let c = dates[2].substring(0,dates[2].length-1)
         var month1;
         var year1;
         if (dates[1] && isNaN(dates[1])) {
           const sd = dates[1];
           month1 = months[sd];
-          year1 = dates[2]?.length > 2 ? dates[2] : !dates[2] ? " " : dates[2];
+          year1 = c?.length > 2 ? c : !c ? " " : c;
         } else {
           month1 =
             dates[0]?.length === 1
@@ -115,14 +132,13 @@ app.post("/upload-csv-file", (req, res) => {
               : !dates[0]
               ? " "
               : dates[0];
-          year1 = dates[2]?.length > 2 ? dates[2] : !dates[2] ? " " : dates[2];
+          year1 = c?.length > 2 ? c : !c ? " " : c;;
         }
         if (month === month1 && year === year1) {
           ans.push(e);
         }
       } else {
         const dates = dates1.toString().split("/");
-        //console.log(dates);
         var month1;
         var year1;
         if (dates[1] && isNaN(dates[1])) {
@@ -142,7 +158,8 @@ app.post("/upload-csv-file", (req, res) => {
           ans.push(e);
         }
       }
-    });
+    }
+  });
   }
 
   res.send(ans);
